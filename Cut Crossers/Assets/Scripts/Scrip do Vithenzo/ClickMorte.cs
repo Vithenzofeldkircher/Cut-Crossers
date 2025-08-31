5921a00856163577300c6a297bae5644c32c7fb1
Vithenzofeldkircher <vithenzo.feldkircher@alunos.sc.senac.br>
2025-08-31 00:00:15 -0300

    Update(vithenzo)
    implementação da musicas e dos upgrades

--- Cut Crossers/Assets/Scripts/Scrip do Vithenzo/ClickMorte.cs
+++ Cut Crossers/Assets/Scripts/Scrip do Vithenzo/ClickMorte.cs
@@ -3,8 +3,8 @@ using TMPro;
 using UnityEngine.UI;
 public class ClickMorte : MonoBehaviour
 {
-    public int contador = 0;    
-    [SerializeField]    TMP_Text CountText;
+        
+    [SerializeField]  TMP_Text CountText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,17 +15,41 @@ public class ClickMorte : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        AtualizarTexto();
     }
 
     public void Clickpositivo()
     {
-        contador++;
-        UpdateUI();
+        GameManager.instance.TotalMortes += GameManager.instance.valorDoClick;
+        AtualizarTexto();
     }
 
-    private void UpdateUI()
+    private void AtualizarTexto()
     {
-        CountText.text = "Mortes: " + contador.ToString();
+        CountText.text = "Mortes: " + GameManager.instance.TotalMortes.ToString();
+    }
+
+    public void upgrade1()
+    {
+
+        int custoUpgrade = 10;
+        if (GameManager.instance.TotalMortes >= custoUpgrade)
+        {
+            GameManager.instance.TotalMortes -= custoUpgrade;
+            GameManager.instance.valorDoClick += 1;
+            AtualizarTexto();
+        }
+    }
+
+    public void upgradePassivo()
+    {
+        int custoUpgradePassivo = 50;
+        if (GameManager.instance.TotalMortes >= custoUpgradePassivo)
+        {
+            GameManager.instance.TotalMortes -= custoUpgradePassivo;
+            GameManager.instance.mortesporsegundo += 1;
+            AtualizarTexto();
+        }
+
     }
 }
